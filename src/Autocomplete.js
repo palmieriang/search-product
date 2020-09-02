25548687eaa95b34f3a3fc75c22f27eb0d837693
Angelo <angelo.palmieri@rufusleonard.com>
2020-09-02 14:51:38 +0100

    Autocomplete - accessibility - tabs

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -27,19 +27,23 @@ const Autocomplete = ({ handleProductId }) => {
   const handleOnKeyDown = (event) => {
     // User pressed the enter key, update the input and close the suggestions
     if (event.keyCode === 13) {
-      handleProductId(suggestions[activeSuggestion].id);
+      if (event.target.dataset.id) {
+        handleProductId(event.target.dataset.id);
+      } else if (suggestions.length > 0) {
+        handleProductId(suggestions[activeSuggestion].id);
+      }
       setSearchTerm('');
       setActiveSuggestion(0);
     }
     // User pressed the up arrow, decrement the index
-    else if (event.keyCode === 38 || (event.shiftKey && event.keyCode === 9)) {
+    else if (event.keyCode === 38) {
       if (activeSuggestion === 0) {
         return;
       }
       setActiveSuggestion(activeSuggestion - 1);
     }
-    // User pressed the down arrow or tab, increment the index
-    else if (event.keyCode === 40 || event.keyCode === 9) {
+    // User pressed the down arrow, increment the index
+    else if (event.keyCode === 40) {
       if (activeSuggestion + 1 === suggestions.length) {
         return;
       }
@@ -60,7 +64,9 @@ const Autocomplete = ({ handleProductId }) => {
 
   return (
     <div className="search-container">
+      <label htmlFor="search-box" className="visually-hidden">Search for a product or brand</label>
       <input
+        id="search-box"
         type="text"
         value={searchTerm}
         className="search-box"
@@ -79,13 +85,20 @@ const Autocomplete = ({ handleProductId }) => {
             }
             return (
               <li
+                data-index={index}
                 key={suggestion.id}
                 className={className}
-                data-index={index}
                 onClick={handleChooseProduct}
-                onMouseEnter={handleMouse}
               >
-                {suggestion.name}
+                <button
+                  data-index={index}
+                  data-id={suggestion.id}
+                  onClick={handleChooseProduct}
+                  onMouseEnter={handleMouse}
+                  onKeyDown={handleOnKeyDown}
+                >
+                  {suggestion.name}
+                </button>
               </li>
             )
           })}

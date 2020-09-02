babe6dfcbbf1f9138182dcb338e533294477e4de
Angelo <angelo.palmieri@rufusleonard.com>
2020-09-02 15:49:15 +0100

    ProductDetail - sorted

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -17,19 +17,21 @@ const ProductDetail = ({ productId }) => {
   }, [productId]);
 
   const renderProductInfo = () => {
+    const {productImage, name, price, productCode} = productInfo;
+
     return (
       <div className="detail-container">
         <div className="row">
-          <img src={productInfo.productImage} className="product-image" alt="" />
+          <img src={productImage} className="product-image" alt={name} />
         </div>
 
         <div className="row">
-          <div className="row-body">{productInfo.name}</div>
+          <div className="row-body">{name}</div>
         </div>
 
         <div className="row">
-          <div className="row-body product-price">{productInfo.price}</div>
-          <div className="row-body product-id">{productInfo.productCode}</div>
+          <div className="row-body product-price">{price}</div>
+          <div className="row-body product-id">{productCode}</div>
         </div>
       </div>
     );

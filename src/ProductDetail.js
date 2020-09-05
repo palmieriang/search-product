43e8e011134026ae47163c7bd5d4faab067b4375
Angelo <angelo.palmieri@rufusleonard.com>
2020-09-05 15:39:26 +0100

    ProductDetail - isLoading - svg

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import { fetchProductDetail } from "./utils/api";
+import { ReactComponent as LoadingIcon } from "./svg/loading.svg";
 
 import "./ProductDetail.css";
 
 const ProductDetail = ({ productId }) => {
   const [productInfo, setProductInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!productId) return;
 
-    fetchProductDetail(productId).then((productInfo) =>
-      setProductInfo(productInfo)
-    );
+    setIsLoading(true);
+    fetchProductDetail(productId).then((productInfo) => {
+      setProductInfo(productInfo);
+      setIsLoading(false);
+    });
   }, [productId]);
 
   if (!productInfo) {
@@ -23,20 +27,32 @@ const ProductDetail = ({ productId }) => {
   const {productImage, name, price, productCode} = productInfo;
 
   return (
-    <div className="detail-container">
-      <div className="row">
-        <img src={productImage} className="product-image" alt={name} />
-      </div>
+    <>
+      {isLoading ?
+        (
+          <div className="loading-container">
+            <LoadingIcon />
+          </div>
+        )
+        :
+        (
+          <div className="detail-container">
+            <div className="row">
+              <img src={productImage} className="product-image" alt={name} />
+            </div>
 
-      <div className="row">
-        <div className="row-body">{name}</div>
-      </div>
+            <div className="row">
+              <div className="row-body">{name}</div>
+            </div>
 
-      <div className="row">
-        <div className="row-body product-price">{price}</div>
-        <div className="row-body product-id">{productCode}</div>
-      </div>
-    </div>
+            <div className="row">
+              <div className="row-body product-price">{price}</div>
+              <div className="row-body product-id">{productCode}</div>
+            </div>
+          </div>
+        )
+      }
+    </>
   );
 }
 

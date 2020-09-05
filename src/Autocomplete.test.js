b887e924518c3f6a591a6e3a9c7705f851cbaeb4
Angelo <angelo.palmieri@rufusleonard.com>
2020-09-05 18:51:17 +0100

    ProductDetail.test

--- src/Autocomplete.test.js
+++ src/Autocomplete.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import {render, screen, fireEvent, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+
 import Autocomplete from "./Autocomplete";
 import { fetchSuggestions } from "./utils/api";
 
---
+++ src/ProductDetail.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import {render, screen, waitFor} from "@testing-library/react";
+import ProductDetail from "./ProductDetail";
+import { fetchProductDetail } from "./utils/api";
+
+jest.mock("./utils/api");
+
+const productId = '1790102';
+const productInfo = {
+  name: "shirt in navy",
+  productCode: "1774327",
+  productImage: "https://asos.com/image.json",
+  price: "£60.00"
+};
+
+describe("ProductDetail", () => {
+  it("renders correctly", () => {
+    const wrapper = shallow(<ProductDetail />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should show product id, name and price", async () => {
+    fetchProductDetail.mockResolvedValueOnce(productInfo);
+
+    render(<ProductDetail productId={productId} />);
+
+    await waitFor(() => expect(screen.getByText(productInfo.name)).toBeInTheDocument());
+    expect(screen.getByText(productInfo.productCode)).toBeInTheDocument();
+    expect(screen.getByText(productInfo.price)).toBeInTheDocument();
+    expect(screen.getByAltText(productInfo.name)).toHaveAttribute('src', productInfo.productImage);
+  });
+});
